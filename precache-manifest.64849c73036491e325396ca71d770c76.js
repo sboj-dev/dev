f46a2cfc3a0d04dc9fa1dab8902f0618ee96dbde
Benji Hooper <benji@sixp.in>
2020-05-06 14:46:42 +0100

    Deploy

---
+++ precache-manifest.64849c73036491e325396ca71d770c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2079e29636bb0ff91603ac92a6153091",
+    "url": "https://sboj.io/dev/index.html"
+  },
+  {
+    "revision": "58c504b56d6850445f10",
+    "url": "https://sboj.io/dev/static/css/main.cfdd6f8b.chunk.css"
+  },
+  {
+    "revision": "ab94289008411ba5da19",
+    "url": "https://sboj.io/dev/static/js/2.f0d94efd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://sboj.io/dev/static/js/2.f0d94efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c504b56d6850445f10",
+    "url": "https://sboj.io/dev/static/js/main.9bf9dc42.chunk.js"
+  },
+  {
+    "revision": "7ffc6aa85c3e3f768065",
+    "url": "https://sboj.io/dev/static/js/runtime-main.ae2dddc9.js"
+  }
+]);
