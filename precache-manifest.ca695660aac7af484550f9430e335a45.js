4519fe34bccc3555e6b10c015ded1ea4dfa04c67
Benji Hooper <benji@sixp.in>
2020-06-08 16:54:30 +0100

    Deploy

--- precache-manifest.ca695660aac7af484550f9430e335a45.js
+++ precache-manifest.ca695660aac7af484550f9430e335a45.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e53da5acb0f7b54b2d0e4215e7c1e5c2",
+    "revision": "b2263900a15fe360ffa07306c053803d",
     "url": "https://sboj.io/dev/index.html"
   },
   {
-    "revision": "c0ed855c94e2ec3c4a27",
-    "url": "https://sboj.io/dev/static/css/main.1be95a26.chunk.css"
+    "revision": "2a5e00f098c77dae3886",
+    "url": "https://sboj.io/dev/static/css/main.b661b989.chunk.css"
   },
   {
     "revision": "4c907390c4664dce6349",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://sboj.io/dev/static/js/2.b15928a1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0ed855c94e2ec3c4a27",
-    "url": "https://sboj.io/dev/static/js/main.f14277d2.chunk.js"
+    "revision": "2a5e00f098c77dae3886",
+    "url": "https://sboj.io/dev/static/js/main.e16462e7.chunk.js"
   },
   {
     "revision": "7ffc6aa85c3e3f768065",
