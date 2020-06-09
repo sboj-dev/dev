e5ff662b1ea7dddc3026d560ee7b74e515631cf1
Benji Hooper <benji@sixp.in>
2020-06-09 15:25:43 +0100

    Deploy

--- precache-manifest.6bc8fd961d94d20d4ed7a92a2d627eb1.js
+++ precache-manifest.6bc8fd961d94d20d4ed7a92a2d627eb1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "34374a5af2cde72654cbb3183c0383fc",
+    "revision": "db725d3fc8dee76b0db0d99814293909",
     "url": "https://sboj.io/dev/index.html"
   },
   {
-    "revision": "9c7402b03cc3b1e92828",
-    "url": "https://sboj.io/dev/static/css/main.881b0be9.chunk.css"
+    "revision": "d7b4a3d0c75f54fc4b5b",
+    "url": "https://sboj.io/dev/static/css/main.1bc4b094.chunk.css"
   },
   {
     "revision": "42776c713773a8a5247a",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://sboj.io/dev/static/js/2.9549c9a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c7402b03cc3b1e92828",
+    "revision": "d7b4a3d0c75f54fc4b5b",
     "url": "https://sboj.io/dev/static/js/main.02fddd88.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://sboj.io/dev/precache-manifest.bb1b97bb209d66c8b83cf76512b1a13f.js"
+  "https://sboj.io/dev/precache-manifest.6bc8fd961d94d20d4ed7a92a2d627eb1.js"
 );
 
 self.addEventListener('message', (event) => {
